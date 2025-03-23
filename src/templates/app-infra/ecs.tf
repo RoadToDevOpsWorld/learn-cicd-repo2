e987db18463aded2bd9d1ee5b2ffd3821aa92a32
ajay <ajaybas141@gmail.com>
2025-03-23 19:24:42 +0530

    Adding cd tf code

--- src/templates/app-infra/ecs.tf
+++ src/templates/app-infra/ecs.tf
@@ -21,3 +21,37 @@ resource "aws_ecs_capacity_provider" "this" {
     }
   }
 }
+
+resource "aws_ecs_task_definition" "service" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "first"
+      image     = "service-first"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 8080
+        }
+      ]
+    }
+  ])
+
+}
+
+resource "aws_ecs_service" "tradapp" {
+  name            = "tradapp"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.service.arn
+  desired_count   = 1
+  depends_on      = [aws_iam_role_policy.foo]
+
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "cpu"
+  }
+
+}
