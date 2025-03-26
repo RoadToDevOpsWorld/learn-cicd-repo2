f6568cbfae0603a325b2edd5641a16c6120df70d
ajay <ajaybas141@gmail.com>
2025-03-26 13:03:32 +0530

    asg tag changed

--- src/templates/app-infra/asg.tf
+++ src/templates/app-infra/asg.tf
@@ -84,8 +84,8 @@ resource "aws_autoscaling_group" "this" {
   }
 
   tag {
-    key                 = "Name"
-    value               = "ASG-${var.env}"
+    key                 = "AmazonECSManaged"
+    value               = true
     propagate_at_launch = true
   }
 }
