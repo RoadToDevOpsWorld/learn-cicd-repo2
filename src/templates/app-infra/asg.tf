0ae4ba8046ef44f0f4b84892eb36d2383d3144d8
Saravanan Baskaran <bprathap104@gmail.com>
2025-03-28 09:47:35 +0530

    Fixing sg rule error

--- src/templates/app-infra/asg.tf
+++ src/templates/app-infra/asg.tf
@@ -114,9 +114,9 @@ resource "aws_vpc_security_group_ingress_rule" "this1" {
 resource "aws_vpc_security_group_egress_rule" "this1" {
   security_group_id = aws_security_group.this2.id
   cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 0
+  from_port   = "-1"
   ip_protocol = "-1"
-  to_port     = 65535
+  to_port     = "-1"
 }
 
 
