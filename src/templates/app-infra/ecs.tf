dafbd5a31f8f89da0617c24eaf39b6ecaf908aa5
Prathap <57014443+bprathap104@users.noreply.github.com>
2025-03-29 07:49:08 +0530

    Update ecs.tf

--- src/templates/app-infra/ecs.tf
+++ src/templates/app-infra/ecs.tf
@@ -106,20 +106,6 @@ resource "aws_security_group" "alb" {
   description = "Security group for ALB"
   vpc_id      = data.aws_vpc.default.id
 
-  ingress {
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = "alb-security-group"
   }
